a4bd62178b97655b4775fcf9591ee2cbac49e23b
Isaac Santana <zrfisaac@gmail.com>
2023-03-08 20:27:20 -0300

    zrfisaac.en.batch.model - 0.0.1

---
+++ en/batch/model.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+rem # [ zrfisaac ]
+
+rem # [ about ]
+rem # - author : Isaac Santana
+rem # . - email : zrfisaac@gmail.com
+rem # . - site : zrfisaac.github.io
+rem # - base : none
+rem # - description : basic batch script model
+rem # - version : zrfisaac.en.batch.model - 0.0.1
+
+rem # [ batch ]
+
+rem # : - start
+echo # [ start ]
+echo # - source : %~0
+echo.
+
+rem # : - main
+if "0" equ "0" (
+	rem # : - title
+	echo # [ main ]
+
+	rem # : - routine
+	if "0" equ "0" (
+		echo # - routine
+	)
+
+	rem # : - end
+	echo.
+)
+
+rem # : - finish
+echo # [ finish ]
+set /p _=
