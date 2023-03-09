a8688153c2b4f133e76aaa080aa60b354e54f699
Isaac Santana <zrfisaac@gmail.com>
2023-03-09 05:20:03 -0300

    zrfisaac.pt.mssql.modelo - 0.0.1

---
+++ pt/mssql/modelo.mssql.sql
@@ -0,0 +1,16 @@
+-- # [ zrfisaac ]
+
+-- # [ sobre ]
+-- # - autor : Isaac Santana
+-- # . - email : zrfisaac@gmail.com
+-- # . - site : zrfisaac.github.io
+-- # - base : nenhum
+-- # - descricao : modelo basico de script batch
+-- # - versao : zrfisaac.pt.mssql.modelo - 0.0.1
+
+-- # [ mssql ]
+SELECT
+	 01
+	AS ID
+	,'VERMELHO'
+	AS NOME
